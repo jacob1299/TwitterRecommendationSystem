00658f4b561cf9998569f6278037e73d5e178233
thomasDevan99 <70350934+thomasDevan99@users.noreply.github.com>
2021-04-14 23:37:31 -0500

    Data set loaded, works ok
    still has a lot of noise and dirty data that i cant clean at the moment but IT WORKS for the most part. Gonna try to make a better searching function.

--- tool/TF-IDF.py
+++ tool/TF-IDF.py
@@ -3,47 +3,78 @@
 """
 Created on Fri Apr  9 18:05:42 2021
 Search Alg
-@author: Thomas Devan
+@author: Robot
 """
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 import re
 import pandas as pd
 import numpy as np
+from math import sqrt
+from array import array
+from scipy.spatial.distance import euclidean
 
-docs = "pizza" , "izz", "pizza 0", "pizza 0 izz", "horse", "cow", "cow dog"
+docs = pd.read_csv("file-name.csv")
+docs = docs["2"]
+spec_chars = ["!",'"',"#","%","&","'","(",")",
+              "*","+",",","-",".","/",":",";","<",
+              "=",">","?","@","[","\\","]","^","_",
+              "`","{","|","}","~","–"]
+
+for char in spec_chars:
+    docs = docs.str.replace(char, ' ')
+## 1 letter words seem to not count as words
 
 vectorizer = TfidfVectorizer()
 
-X = vectorizer.fit_transform(docs)
+tf_idf = vectorizer.fit_transform(docs)
 
-X = X.T.toarray()
+tf_idf = tf_idf.T.toarray()
 
-df = pd.DataFrame(X, index=vectorizer.get_feature_names())
+df = pd.DataFrame(tf_idf, index=vectorizer.get_feature_names())
+
+rows = len(df.columns)
+
 
 def get_similar_tweets(q, df) :
     print("query:", q)
-    print("Cosin similarity:")
+    
     
     q = [q]
     q_vector = vectorizer.transform(q).toarray().reshape(df.shape[0],)
     sim = {}
-    rows = len(df.columns)
+    
+    
+    print("Cosin similarity:")
     
     for i in range(rows):
         sim[i] = np.dot(df.loc[:,i].values, q_vector)/np.linalg.norm(df.loc[:, i])*np.linalg.norm(q_vector)
         
-        sim_sorted = sorted(sim.items(), key=lambda x: x[1], reverse= True)
+        sim_sorted = sorted(sim.items(), key = lambda x: x[1], reverse = False)
         
 
     
-    for k, v in sim_sorted:
+    for k, v in sim_sorted:         
         if v != 0.0 and v!= None :
             print("Similarity Values: ", v)
             print(docs[k])
             print()
-                
+
+        
             
+        
+#def euclidean(df):
+#    m = 0
+#    for j in df:
+#        m = m+1
+#        dist = array('l')
+#        dist = dist.append(euclidean(df[m],df[m-1]))
+#    print(dist)
+#    return dist
+#    print(dist)
+    
 q1 = input("Enter your search: ")
 get_similar_tweets(q1, df)
+##euclidean(df)
+
 
